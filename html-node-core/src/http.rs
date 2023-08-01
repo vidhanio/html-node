09198e75a830578da6cb37c0b79f74da18d08807
Conner Blair <conner@probablyyes.software>
2023-07-31 23:16:43 -0700

    Add axum support for pretty wrapper.

--- html-node-core/src/http.rs
+++ html-node-core/src/http.rs
@@ -2,9 +2,15 @@
 mod axum {
     use axum::response::{Html, IntoResponse, Response};
 
-    use crate::Node;
+    use crate::{pretty::Pretty, Node};
 
     impl IntoResponse for Node {
+        fn into_response(self) -> Response {
+            Html(self.to_string()).into_response()
+        }
+    }
+
+    impl IntoResponse for Pretty {
         fn into_response(self) -> Response {
             Html(self.to_string()).into_response()
         }
